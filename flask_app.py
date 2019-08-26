478c6ba99a1ac720c2584d6c916cf5b1552a4de5
Neha Tamore <nehatamore87@gmail.com>
2019-08-27 01:04:25 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,26 +1,23 @@
-import json
-import os
 from flask import Flask,jsonify,request
 from flask_cors import CORS
-from model import spell_model
+from model import spell_corrector
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/spellCorrect/<word>')
-def return_price(word):
-  #word = request.args.get('word')
-  model = spell_model()
-  corrected_words  = model.predict(word)
+@app.route("/spellCorrect/<word>")
+def return_words(word):
+  # word = request.args.get('word')
+  spell_corrector_object = spell_corrector()
+  corrected_words  = spell_corrector_object.predict(word)
+  corrected_words = ' '.join(corrected_words)
   output_dict = {
                 'corrected_words': corrected_words
                 }
-  print(output_dict)
-  #return jsonify(output_dict)
-
+  return jsonify(output_dict)
 
 @app.route("/",methods=['GET'])
 def default():
-    return "<h1> Welcome to spell corrector <h1>"
+  return "<h1> Welcome to spell corrector <h1>"
 
 if __name__ == "__main__":
     app.run()
