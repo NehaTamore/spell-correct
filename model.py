c1897f9d083a2720060143ccbbf1f0ed54ae9700
Neha Tamore <nehatamore87@gmail.com>
2019-08-27 01:05:06 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,12 +5,6 @@ class spell_model:
     def __init__(self):
         pass
 
-    def deserialize(self):
-        # de-serialize spell_corrector.pkl file into an object called model using pickle
-        with open('spell_corrector.pkl', 'rb') as handle:
-            model = pickle.load(handle)
-        return model
-
     def predict(self, word):
         return correction(word)
 
